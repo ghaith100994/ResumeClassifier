0b8dc893f139f2aea16d9c27891d5a4643bafc58
IyadAbuMustafa <eng.iyad95@gmail.com>
2019-07-05 19:52:47 +0300

    Update 'ResumeHelper' and 'ResumeSegmenter' documentation and added 'updatenormalize' function to 'ResumeSegmenter' file

--- ResumeSegmenter.py
+++ ResumeSegmenter.py
@@ -1,3 +1,4 @@
+import nltk
 import csv
 import re
 import pdfminer
@@ -13,7 +14,7 @@ from pdfminer.converter import PDFPageAggregator,TextConverter
 
 #This function takes one paramter which is a file.
 #Opens this file as 'CSV' file.
-#Returns the keywords of the table header as a list of strings.
+#Returns the first column keywords from the table as a list of strings.
 def read_csv_keywords(input_file):
     with open(input_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -33,14 +34,14 @@ def read_csv(input_file):
 #This function takes one paramter which is a 'PDF' file.
 #Fetches the data from this file using (PDFParser) library.
 #Stores the parsing result into a ducument using (PDFDocument) library.
-#Stores shared resources of this file such as fonts using (PDFResourceManager) library.
-#Translates the PDFResourceManager result into charts using (PDFDevice) library.
+#Stores shared resources of this file such as font's size and type using (PDFResourceManager) library.
+#Translates the PDFResourceManager result into layouts using (PDFDevice) library.
 #Process the page contents using (PDFPageInterpreter) library.
-#Converts the document which contains the data into pages using (PDFPage) library, (create_pages) function for sure.
-#Process each page separately, and for each page it process each line separately, and for each line it process each sentence separately.
-#Returns a dictionary mapping between each sentence number and its words in the same chart.
-def read_pdf_miner_charts(fileObj):
-    charts_dict={}
+#Converts the document which contains the data into pages using (PDFPage) library, (create_pages) function for specific.
+#for each line in the PDF it reads char by char.
+#Returns each char with its font size and type.
+def read_pdf_miner_chars(fileObj):
+    chars_dict={}
     file_pointer = open(fileObj,'rb')
     parser = PDFParser(file_pointer)
     document = PDFDocument(parser)
@@ -55,7 +56,7 @@ def read_pdf_miner_charts(fileObj):
     page_num = 1
     id = 0
     sentences_num=0
-    charts=[]
+    chars=[]
     sentences=[]
     for page in PDFPage.create_pages(document):
         interpreter.process_page(page)
@@ -68,15 +69,15 @@ def read_pdf_miner_charts(fileObj):
                     if isinstance(line_obj,pdfminer.layout.LTTextLine):
                         sent=line_obj.get_text()
                         if sent.strip():
-                            charts.clear()
-                            for chart_obj in  line_obj._objs:
-                                if isinstance(chart_obj, pdfminer.layout.LTChar):
-                                    charts.append([chart_obj.get_text(),int(chart_obj.size),chart_obj.fontname])
-                            sentences.append(charts.copy())
-                charts_dict[sentences_num]=sentences.copy()
+                            chars.clear()
+                            for char_obj in  line_obj._objs:
+                                if isinstance(char_obj, pdfminer.layout.LTChar):
+                                    chars.append([char_obj.get_text(),int(char_obj.size),char_obj.fontname])
+                            sentences.append(chars.copy())
+                chars_dict[sentences_num]=sentences.copy()
                 sentences_num+=1
         page_num += 1
-    return charts_dict
+    return chars_dict
 
 #This function takes one paramter which is a 'PDF' file.
 #Fetches the data from this file using (PDFParser) library.
@@ -85,8 +86,8 @@ def read_pdf_miner_charts(fileObj):
 #Translates the PDFResourceManager result into charts using (PDFDevice) library.
 #Process the page contents using (PDFPageInterpreter) library.
 #Converts the document which contains the data into pages using (PDFPage) library, (create_pages) function for sure.
-#Process each page separately, and for each page it process each layout separately, and for each layout it process each sentence separately.
-#Returns a dictionary mapping between a unique id number and the text words of the same layout.
+#for each sentence in the PDF it reads this sentence.
+#Returns the PDF file as list of lists.
 def read_pdf_miner_texts(fileObj):
     #text_prop_dict = {}
     text_dict={}
@@ -120,11 +121,11 @@ def read_pdf_miner_texts(fileObj):
 #Translates the PDFResourceManager result into charts using (PDFDevice) library.
 #Process the page contents using (PDFPageInterpreter) library.
 #Converts the document which contains the data into pages using (PDFPage) library, (create_pages) function for sure.
-#Process each page separately, and for each page it process each line separately, and for each line it process each sentence separately.
-#Returns a dictionary mapping between the page number and the text words of the same layout in this page.
-def read_pdf_miner_pages(fileObj):
+#for each object layout in the PDF it stores the object and its dimensions.
+#Returns each object with its dimensions.
+def read_pdf_miner_layouts(fileObj):
     text_prop_dict = {}
-    page_dict={}
+    layout_dict={}
     file_pointer = open(fileObj,'rb')
     parser = PDFParser(file_pointer)
     document = PDFDocument(parser)
@@ -136,7 +137,7 @@ def read_pdf_miner_pages(fileObj):
     laparams = LAParams()
     device = PDFPageAggregator(resourceManager, laparams=laparams)
     interpreter = PDFPageInterpreter(resourceManager, device)
-    page_num = 1
+    layout_num = 1
     id = 0
     for page in PDFPage.create_pages(document):
         interpreter.process_page(page)
@@ -145,14 +146,14 @@ def read_pdf_miner_pages(fileObj):
             if isinstance(lt_obj, LTTextBoxHorizontal):
                 text_prop_dict[id] = lt_obj
                 id += 1
-        page_dict[page_num]=text_prop_dict.copy()
+        layout_dict[layout_num]=text_prop_dict.copy()
         text_prop_dict.clear()
-        page_num += 1
-    return page_dict
+        layout_num += 1
+    return layout_dict
 
 #This function takes one parameter which is an unnormalaized 'PDF' dictionary.
-#Normalaizes chars array of this dictionary by looking to the previous char 'Size' and the previous char 'Font'.
-#Returns normalaized 'PDF' dictionary.
+#Normalaizes chars array of this dictionary by looking to the previous char 'Font Size' and 'Font Type' and assemble them into one word.
+#Returns normalaized 'PDF' dictionary which contains each sentence and its font size and type.
 def text_normalize(unnormlaize_pdf_dict):
     normlaize_pdf_dict={}
     for chars_arry in unnormlaize_pdf_dict.items():
@@ -172,8 +173,20 @@ def text_normalize(unnormlaize_pdf_dict):
     return normlaize_pdf_dict
 
 #This function takes one parameter which is an normalaized 'PDF' dictionary.
+#Removes the '/n' character.
+#Returns the sentences which belongs to the same block as a list sparated by ','.
+def update_text_normalize(normlaize_pdf_dict):
+    for sent in normlaize_pdf_dict:
+        for sub_sent in normlaize_pdf_dict[sent]:
+            line=sub_sent[0]
+            line=re.sub('\n','',line)
+            lines=nltk.sent_tokenize(line)
+            sub_sent[0]=lines.copy()
+            lines.clear()
+
+#This function takes one parameter which is an normalaized 'PDF' dictionary.
 #Process each paragraph in this dictionary separately, and process each sentence of the paragraph separately.
-#Returns all text sizes in this 'PDF' dictionary as a dictionary mapping between each sentence and its font size.
+#Returns all text font sizes in this 'PDF' dictionary as a dictionary mapping between each sentence and its font size.
 def find_allsize_in_text(normlaize_pdf_dict):
     sizes=defaultdict(int)
     for paragraph in normlaize_pdf_dict.values():
@@ -192,6 +205,7 @@ def find_allfont_in_text(normlaize_pdf_dict):
             if(not sent[0].isspace()):
                 fonts[sent[2]]+=1
     return fonts
+    
 
 work_experience_segment=read_csv_keywords('wordList\\work_experience_segment.csv')
 skill_segment=read_csv_keywords('wordList\\skill_segment.csv')
